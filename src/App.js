dd16d5995a51ba93f2dd7fe0164f52f712d1106a
PabloNunezRuiz <pablo.nunez.ruiz@gmail.com>
2021-06-29 12:34:25 +0200

    First Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
 
 function App() {
+
+  // Definir la categoría
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      
+      const url = `https://gnews.io/api/v4/top-headlines?topic=${categoria}&country=de&token=fb2efc68644ff381336da9d79a30a07b`;
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+      
+      guardarNoticias(noticias.articles);
+    }
+    consultarAPI();
+  }, [categoria]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header 
+        titulo="News Search Engine"
+      />
+      <div className="container white">
+        <Formulario 
+          guardarCategoria={guardarCategoria}
+        />
+        <ListadoNoticias 
+          noticias={noticias}
+        />
+      </div>
+    </Fragment>
+    
   );
 }
 
